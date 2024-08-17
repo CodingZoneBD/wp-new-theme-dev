57df380ab5399972ef0a411bdefaeaf0e7d7e141
Dalawer Hossain Juwel <juwel@namespaceit.com>
2024-08-17 16:03:34 +0600

    Footer wigets add

--- inc/widgets_register.php
+++ inc/widgets_register.php
@@ -11,6 +11,36 @@ function nsit_widgets_register()
         'before_title' => '<h2 class="title">',
         'after_title' => '</h2>',
     ));
+
+    register_sidebar(array(
+        'name' => __('Footer 1', 'artseaiartgenerator'),
+        'id' => 'footer-1',
+        'description' => __('Appears in the sidebar', 'artseaiartgenerator'),
+        'before_widget' => '<div class="child_sidebar">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2 class="title">',
+        'after_title' => '</h2>',
+    ));
+
+    register_sidebar(array(
+        'name' => __('Footer 2', 'artseaiartgenerator'),
+        'id' => 'footer-2',
+        'description' => __('Appears in the sidebar', 'artseaiartgenerator'),
+        'before_widget' => '<div class="child_sidebar">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2 class="title">',
+        'after_title' => '</h2>',
+    ));
+
+    register_sidebar(array(
+        'name' => __('Footer 3', 'artseaiartgenerator'),
+        'id' => 'footer-3',
+        'description' => __('Appears in the sidebar', 'artseaiartgenerator'),
+        'before_widget' => '<div class="child_sidebar">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2 class="title">',
+        'after_title' => '</h2>',
+    ));
 }
 
 add_action('widgets_init', 'nsit_widgets_register');
