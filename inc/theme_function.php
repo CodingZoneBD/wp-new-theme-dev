225b033325462b8e5b0dc9712d62576db13b1c66
Dalawer Hossain Juwel <juwelrana090@gmail.com>
2024-08-19 01:13:13 +0600

    Themes Color add

--- inc/theme_function.php
+++ inc/theme_function.php
@@ -61,6 +61,50 @@ function nsit_customizar_register($wp_customize)
             'center_menu' => __('Center Menu', 'artseaiartgenerator'),
         )
     )));
+
+    // Theme Color
+    $wp_customize->add_section('nsit_theme_color', array(
+        'title' => __('Theme Color', 'artseaiartgenerator'),
+        'description' => __('You can change the theme color', 'artseaiartgenerator'),
+    ));
+
+    $wp_customize->add_setting('nsit_bg_color', array(
+        'default' => '#ffffff',
+    ));
+
+    $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'nsit_bg_color', array(
+        'label' => __('Background Color', 'artseaiartgenerator'),
+        'description' => __('You can change the background color', 'artseaiartgenerator'),
+        'section' => 'nsit_theme_color',
+        'settings' => 'nsit_bg_color',
+    )));
+
+    $wp_customize->add_setting('nsit_primary_color', array(
+        'default' => '#ea1a70',
+    ));
+
+    $wp_customize->add_control(new WP_Customize_Color_Control($wp_customize, 'nsit_primary_color', array(
+        'label' => __('Primary Color', 'artseaiartgenerator'),
+        'description' => __('You can change the link color', 'artseaiartgenerator'),
+        'section' => 'nsit_theme_color',
+        'settings' => 'nsit_primary_color',
+    )));
 }
 
 add_action('customize_register', 'nsit_customizar_register');
+
+
+function nsit_theme_color_cus()
+{
+?>
+    <style>
+        body {
+            background: <?php echo get_theme_mod('nsit_bg_color'); ?>
+        }
+        :root {
+            --primary: <?php echo get_theme_mod('nsit_primary_color'); ?>
+        }
+    </style>
+<?php
+}
+add_action('wp_head', 'nsit_theme_color_cus');
