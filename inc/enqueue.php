8fc2e836c561bf87476adb65cf89f5ca15c5d6fd
Dalawer Hossain Juwel <juwelrana090@gmail.com>
2024-08-19 05:09:17 +0600

    owl-carousel add

--- inc/enqueue.php
+++ inc/enqueue.php
@@ -5,9 +5,14 @@ function nsit_css_js_files_call()
     // Style
     wp_enqueue_style('nsit-style', get_stylesheet_uri());
     wp_register_style('bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.min.css', array(), '5.3.3', 'all');
+    wp_register_style('owl-carousel', get_template_directory_uri().'/assets/css/owl.carousel.min.css', array(), '2.3.4', 'all');
+    wp_register_style('owl-theme-default', get_template_directory_uri().'/assets/css/owl.theme.default.min.css', array(), '2.3.4', 'all');
     wp_register_style('bxslider', get_template_directory_uri() . '/assets/css/bxslider.css', array(), '4.2.17', 'all');
     wp_register_style('custom', get_template_directory_uri() . '/assets/css/custom.css', array(), '1.0.0', 'all');
+    
     wp_enqueue_style('bootstrap');
+    wp_enqueue_style('owl-carousel');
+    wp_enqueue_style('owl-theme-default');
     wp_enqueue_style('bxslider');
     wp_enqueue_style('custom');
 
@@ -15,6 +20,7 @@ function nsit_css_js_files_call()
     wp_enqueue_script('jquery');
     wp_enqueue_script('bootstrap', get_template_directory_uri() . '/assets/js/bootstrap.bundle.js', array(), '5.3.3', true);
     wp_enqueue_script('bxslider', get_template_directory_uri() . '/assets/js/bxslider.min.js', array(), '5.3.3', true);
+    wp_enqueue_script('owl-carousel', get_template_directory_uri().'/assets/js/owl.carousel.min.js', array(), '2.3.4', 'true' );
     wp_enqueue_script('main', get_template_directory_uri() . '/assets/js/main.js', array(), '1.0.0', true);
 }
 
