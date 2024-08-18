2be17b6341fda941394e885daed188f017c80c45
Dalawer Hossain Juwel <juwelrana090@gmail.com>
2024-08-19 05:19:18 +0600

    home page update

--- inc/widgets_register.php
+++ inc/widgets_register.php
@@ -41,6 +41,16 @@ function nsit_widgets_register()
         'before_title' => '<h2 class="title">',
         'after_title' => '</h2>',
     ));
+
+    register_sidebar(array(
+        'name' => __('Home Page widget', 'artseaiartgenerator'),
+        'id' => 'home-1',
+        'description' => __('Appears in the sidebar', 'artseaiartgenerator'),
+        'before_widget' => '<div class="child_home">',
+        'after_widget' => '</div>',
+        'before_title' => '<h2 class="title">',
+        'after_title' => '</h2>',
+    ));
 }
 
 add_action('widgets_init', 'nsit_widgets_register');
