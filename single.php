e47c75d0dc33b951d10806317410db3b66e8253a
Dalawer Hossain Juwel <juwelrana090@gmail.com>
2024-08-19 05:35:32 +0600

    comments update

--- single.php
+++ single.php
@@ -1,24 +1,26 @@
 <?php
 /*
 * The template for displaying pages
-*/ 
+*/
 get_header(); ?>
 
-  <section id="body_area">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-9 post_page">
-            <?php get_template_part('templates_parts/post_setup'); ?>
+<section id="body_area">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-9 post_page">
+        <?php get_template_part('templates_parts/post_setup'); ?>
 
-            <div id="comments_area">
-              <?php comments_template(); ?>
-            </div>
-        </div>
-        <div class="col-md-3">
-          <?php get_sidebar(); ?>
+        <div id="comments_area">
+          <?php if (comments_open()) : ?>
+            <?php comments_template(); ?>
+          <?php endif; ?>
         </div>
       </div>
+      <div class="col-md-3">
+        <?php get_sidebar(); ?>
+      </div>
     </div>
-  </section>
+  </div>
+</section>
 
-  <?php get_footer(); ?>
+<?php get_footer(); ?>
